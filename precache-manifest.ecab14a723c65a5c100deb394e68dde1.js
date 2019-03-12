9586a92f25c73368ca1f7bbde3db494029a05913
Andrey <andrey.zertsalov@phystech.edu>
2019-03-12 16:01:22 +0300

    Updates

---
+++ precache-manifest.ecab14a723c65a5c100deb394e68dde1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5b96a88a1f3f17ccba2",
+    "url": "/dendrogram/static/css/main.854d1dee.chunk.css"
+  },
+  {
+    "revision": "d5b96a88a1f3f17ccba2",
+    "url": "/dendrogram/static/js/main.3f686900.chunk.js"
+  },
+  {
+    "revision": "e82c18ae0a020fae7638",
+    "url": "/dendrogram/static/js/runtime~main.9f6145c9.js"
+  },
+  {
+    "revision": "9d608fe2e1617daa9b7b",
+    "url": "/dendrogram/static/js/2.17e99b96.chunk.js"
+  },
+  {
+    "revision": "416b93d4aa6d8d727a0f71450084edbc",
+    "url": "/dendrogram/index.html"
+  }
+];
